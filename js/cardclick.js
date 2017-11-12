475ecbd9d6f82d4819b1360c68e8402aff431edd
Lyanf <609136080@qq.com>
2017-11-12 11:42:54 +0800

    Initial commit

---
+++ js/cardclick.js
@@ -0,0 +1,28 @@
+
+$("#card1").click(function () {
+    $("#pic1").click();
+});
+$("#card2").click(function () {
+    $("#pic2").click();
+});
+$("#card3").click(function () {
+    $("#pic3").click();
+});
+$("#card4").click(function () {
+    $("#pic4").click();
+});
+$("#card5").click(function () {
+    $("#pic5").click();
+});
+$("#card6").click(function () {
+    $("#pic6").click();
+});
+$("#card7").click(function () {
+    $("#pic7").click();
+});
+$("#card8").click(function () {
+    $("#pic8").click();
+});
+$("#card9").click(function () {
+    $("#pic9").click();
+});
